084b7ca510d853e0cbe284f1ee50938b3c94fe2e
vanakraj <a3z4km1337@gmail.com>
2025-09-01 23:44:15 +0200

    minor changes, just everything everywhere at once

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Psychic_train_terry_was_right
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
+            menuStrip1 = new MenuStrip();
+            booksToolStripMenuItem = new ToolStripMenuItem();
+            addNewBookToolStripMenuItem = new ToolStripMenuItem();
+            viewBooksToolStripMenuItem = new ToolStripMenuItem();
+            studentToolStripMenuItem = new ToolStripMenuItem();
+            addStudentToolStripMenuItem = new ToolStripMenuItem();
+            viewStudentInfoToolStripMenuItem = new ToolStripMenuItem();
+            issueBookToolStripMenuItem = new ToolStripMenuItem();
+            returnBookToolStripMenuItem = new ToolStripMenuItem();
+            copleteToolStripMenuItem = new ToolStripMenuItem();
+            bonusToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.Indigo;
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { booksToolStripMenuItem, studentToolStripMenuItem, issueBookToolStripMenuItem, returnBookToolStripMenuItem, copleteToolStripMenuItem, bonusToolStripMenuItem, exitToolStripMenuItem, toolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(9, 3, 0, 3);
+            menuStrip1.Size = new Size(1286, 60);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // booksToolStripMenuItem
+            // 
+            booksToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { addNewBookToolStripMenuItem, viewBooksToolStripMenuItem });
+            booksToolStripMenuItem.ForeColor = Color.White;
+            booksToolStripMenuItem.Image = (Image)resources.GetObject("booksToolStripMenuItem.Image");
+            booksToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            booksToolStripMenuItem.Name = "booksToolStripMenuItem";
+            booksToolStripMenuItem.Size = new Size(127, 54);
+            booksToolStripMenuItem.Text = "Books";
+            // 
+            // addNewBookToolStripMenuItem
+            // 
+            addNewBookToolStripMenuItem.Image = (Image)resources.GetObject("addNewBookToolStripMenuItem.Image");
+            addNewBookToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            addNewBookToolStripMenuItem.Name = "addNewBookToolStripMenuItem";
+            addNewBookToolStripMenuItem.Size = new Size(260, 60);
+            addNewBookToolStripMenuItem.Text = "Add New Book";
+            addNewBookToolStripMenuItem.Click += addNewBookToolStripMenuItem_Click;
+            // 
+            // viewBooksToolStripMenuItem
+            // 
+            viewBooksToolStripMenuItem.Image = (Image)resources.GetObject("viewBooksToolStripMenuItem.Image");
+            viewBooksToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            viewBooksToolStripMenuItem.Name = "viewBooksToolStripMenuItem";
+            viewBooksToolStripMenuItem.Size = new Size(260, 60);
+            viewBooksToolStripMenuItem.Text = "View Books";
+            // 
+            // studentToolStripMenuItem
+            // 
+            studentToolStripMenuItem.BackColor = Color.DarkSlateBlue;
+            studentToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { addStudentToolStripMenuItem, viewStudentInfoToolStripMenuItem });
+            studentToolStripMenuItem.ForeColor = Color.White;
+            studentToolStripMenuItem.Image = (Image)resources.GetObject("studentToolStripMenuItem.Image");
+            studentToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            studentToolStripMenuItem.Name = "studentToolStripMenuItem";
+            studentToolStripMenuItem.Size = new Size(139, 54);
+            studentToolStripMenuItem.Text = "Student";
+            // 
+            // addStudentToolStripMenuItem
+            // 
+            addStudentToolStripMenuItem.Image = (Image)resources.GetObject("addStudentToolStripMenuItem.Image");
+            addStudentToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            addStudentToolStripMenuItem.Name = "addStudentToolStripMenuItem";
+            addStudentToolStripMenuItem.Size = new Size(280, 60);
+            addStudentToolStripMenuItem.Text = "Add Student";
+            // 
+            // viewStudentInfoToolStripMenuItem
+            // 
+            viewStudentInfoToolStripMenuItem.Image = (Image)resources.GetObject("viewStudentInfoToolStripMenuItem.Image");
+            viewStudentInfoToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            viewStudentInfoToolStripMenuItem.Name = "viewStudentInfoToolStripMenuItem";
+            viewStudentInfoToolStripMenuItem.Size = new Size(280, 60);
+            viewStudentInfoToolStripMenuItem.Text = "View Student Info";
+            // 
+            // issueBookToolStripMenuItem
+            // 
+            issueBookToolStripMenuItem.ForeColor = Color.White;
+            issueBookToolStripMenuItem.Image = (Image)resources.GetObject("issueBookToolStripMenuItem.Image");
+            issueBookToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            issueBookToolStripMenuItem.Name = "issueBookToolStripMenuItem";
+            issueBookToolStripMenuItem.Size = new Size(164, 54);
+            issueBookToolStripMenuItem.Text = "Issue Book";
+            // 
+            // returnBookToolStripMenuItem
+            // 
+            returnBookToolStripMenuItem.BackColor = Color.DarkSlateBlue;
+            returnBookToolStripMenuItem.ForeColor = Color.White;
+            returnBookToolStripMenuItem.Image = (Image)resources.GetObject("returnBookToolStripMenuItem.Image");
+            returnBookToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            returnBookToolStripMenuItem.Name = "returnBookToolStripMenuItem";
+            returnBookToolStripMenuItem.Size = new Size(175, 54);
+            returnBookToolStripMenuItem.Text = "Return Book";
+            // 
+            // copleteToolStripMenuItem
+            // 
+            copleteToolStripMenuItem.ForeColor = Color.White;
+            copleteToolStripMenuItem.Image = (Image)resources.GetObject("copleteToolStripMenuItem.Image");
+            copleteToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            copleteToolStripMenuItem.Name = "copleteToolStripMenuItem";
+            copleteToolStripMenuItem.Size = new Size(259, 54);
+            copleteToolStripMenuItem.Text = "Complete Book Details";
+            // 
+            // bonusToolStripMenuItem
+            // 
+            bonusToolStripMenuItem.BackColor = Color.DarkSlateBlue;
+            bonusToolStripMenuItem.ForeColor = Color.White;
+            bonusToolStripMenuItem.Image = (Image)resources.GetObject("bonusToolStripMenuItem.Image");
+            bonusToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            bonusToolStripMenuItem.Name = "bonusToolStripMenuItem";
+            bonusToolStripMenuItem.Size = new Size(141, 54);
+            bonusToolStripMenuItem.Text = "Bonus";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Alignment = ToolStripItemAlignment.Right;
+            exitToolStripMenuItem.BackColor = Color.Indigo;
+            exitToolStripMenuItem.ForeColor = Color.White;
+            exitToolStripMenuItem.Image = (Image)resources.GetObject("exitToolStripMenuItem.Image");
+            exitToolStripMenuItem.ImageScaling = ToolStripItemImageScaling.None;
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(105, 54);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(16, 54);
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1286, 785);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Dashboard";
+            Text = "Dashboard";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem booksToolStripMenuItem;
+        private ToolStripMenuItem addNewBookToolStripMenuItem;
+        private ToolStripMenuItem viewBooksToolStripMenuItem;
+        private ToolStripMenuItem studentToolStripMenuItem;
+        private ToolStripMenuItem addStudentToolStripMenuItem;
+        private ToolStripMenuItem viewStudentInfoToolStripMenuItem;
+        private ToolStripMenuItem issueBookToolStripMenuItem;
+        private ToolStripMenuItem returnBookToolStripMenuItem;
+        private ToolStripMenuItem copleteToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem bonusToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+    }
+}
---
+++ Dashboard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Psychic_train_terry_was_right
+{
+    public partial class Dashboard : Form
+    {
+        //No duplicate forms allowed, opens the form if not already open
+        private void OpenForm<T>() where T : Form, new()
+        {
+            
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is T)
+                {
+                    form.Activate(); 
+                    return;
+                }
+            }
+
+            // If no instance is found, create a new one
+            T newForm = new T();
+            newForm.MdiParent = this; 
+            newForm.Show();
+        }
+        public Dashboard()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to leave this world?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void addNewBookToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenForm<AddBooks>();
+        }
+    }
+}
